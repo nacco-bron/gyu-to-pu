a6969fe4ab2ae59a3413ff4146a33816cb822d87
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-05-18 03:13:28 +0900

    実装。ランダムにViewを表示する部分を作成。

--- app/src/main/java/com/example/gyutopu/MainActivity.java
+++ app/src/main/java/com/example/gyutopu/MainActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +20,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btn = findViewById(R.id.messageButton);
-        final TextView messageTextView = findViewById(R.id.messageTextView);
+
+        final List<View> gyuResults = listGyuResults();
+
         btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                String text = event.getAction() == MotionEvent.ACTION_DOWN ? "＼ ﾌﾟｯ ／" : "…";
-                messageTextView.setText(text);
+                if (event.getAction() != MotionEvent.ACTION_DOWN) return false;
+                View gyuResult = randomChoice(gyuResults);
+                showOf(gyuResult, gyuResults);
                 return false;
             }
         });
+    }
+
+    private List<View> listGyuResults() {
+        List<View> results = new ArrayList<>();
+        results.add(findViewById(R.id.messageTextView));
+        results.add(findViewById(R.id.cowFaceImageView));
+        results.add(findViewById(R.id.pigFaceImageView));
+        results.add(findViewById(R.id.hamburgerImageView));
+        results.add(findViewById(R.id.sushiImageView));
+        return results;
+    }
+
+    private View randomChoice(List<View> list) {
+        int index = new Random().nextInt(list.size());
+        return list.get(index);
+    }
+
+    private void showOf(View gyuResult, List<View> gyuResults) {
+        for (View item : gyuResults) {
+            int visible = item == gyuResult ? View.VISIBLE : View.INVISIBLE;
+            item.setVisibility(visible);
+        }
     }
 }
