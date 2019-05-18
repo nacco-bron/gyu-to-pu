df793fbef5b7b5f87e7ee9236051a9bc80ea0d96
nacco-bron <nacco.bron@gmal.com>
2019-05-19 01:54:17 +0900

    #1 Property Animation のサンプル

--- app/src/main/java/com/example/gyutopu/MainActivity.java
+++ app/src/main/java/com/example/gyutopu/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.gyutopu;
 
+import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,16 +23,33 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = findViewById(R.id.messageButton);
 
+        final View animeTextView = findViewById(R.id.animeTextView);
+
         final GyuResults gyuResults = new GyuResults(listGyuResults());
 
         btn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 if (event.getAction() != MotionEvent.ACTION_DOWN) return false;
-                gyuResults.showRandom();
+
+                    animateAlpha(animeTextView);
+
+//                gyuResults.showRandom();
                 return false;
             }
         });
+    }
+
+    private void animateAlpha( View target ) {
+
+        // alphaプロパティを0fから1fに変化させます
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat( target, "alpha", 0f, 1f );
+
+        // 3秒かけて実行させます
+        objectAnimator.setDuration( 3000 );
+
+        // アニメーションを開始します
+        objectAnimator.start();
     }
 
     private List<View> listGyuResults() {
